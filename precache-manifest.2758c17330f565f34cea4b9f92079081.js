c40eedadf16733d8478dadf39ae45437703ee9c6
Vipin Kumar <vipikuma@redhat.com>
2020-01-26 19:19:24 +0530

    Updates

---
+++ precache-manifest.2758c17330f565f34cea4b9f92079081.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb41e3501743e37db76e34101dff0dae",
+    "url": "/my_react_app/index.html"
+  },
+  {
+    "revision": "ede7e43a0e81efde2490",
+    "url": "/my_react_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "abdf7cba5e042577736e",
+    "url": "/my_react_app/static/js/2.8e4dd888.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my_react_app/static/js/2.8e4dd888.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ede7e43a0e81efde2490",
+    "url": "/my_react_app/static/js/main.fa9295ac.chunk.js"
+  },
+  {
+    "revision": "d9ba580cbf1306799a46",
+    "url": "/my_react_app/static/js/runtime-main.ac369644.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_react_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
